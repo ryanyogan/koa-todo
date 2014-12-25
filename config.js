aaab3c50c87c36dcac2f0dff117d82f3da7fa0da
Ryan Yogan <ryanyogan@gmail.com>
2014-12-25 15:03:46 -0500

    Added configuration file

---
+++ config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var version = require('./package.json').version;
+var path    = require('path');
+
+var config = {
+  version: version,
+  debug:   process.env.NODE_ENV !== 'production',
+  port:    process.env.PORT || 7001
+};
+
+module.exports = config;
